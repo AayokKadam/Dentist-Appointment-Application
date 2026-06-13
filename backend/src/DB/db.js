186acfc26252d417119618089af24daaf00edb0c
AayokKadam <aayokkadam@gmail.com>
2026-06-13 10:08:55 +0530

    Update db.js

--- backend/src/DB/db.js
+++ backend/src/DB/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
 
         await mongoose.connect(
-            "mongodb+srv://firstdatabse:itUSrmCn8dlgnzT3@cluster0.a6pj9be.mongodb.net/?appName=Cluster0"
+            "a3f7c2d9e8b4f1a6c5d7e9b2a1f3c4d5e6f7a8b9"
         );
 
         console.log("MongoDB Connected Successfully");
