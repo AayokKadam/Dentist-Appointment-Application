347fa566d1e80a68859bfb54bd2e6c3ac159666f
AayokKadam <aayokkadam@gmail.com>
2026-06-13 10:15:58 +0530

    Update db.js

--- backend/src/DB/db.js
+++ backend/src/DB/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
 
         await mongoose.connect(
-            "a3f7c2d9e8b4f1a6c5d7e9b2a1f3c4d5e6f7a8b9"
+            ""
         );
 
         console.log("MongoDB Connected Successfully");
